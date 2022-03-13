23a5c466d1c25e716fa3e8e0ea29fda68b974b7d
T <totran0331@gmail.com>
2022-03-12 19:12:03 -0500

    Added documentation and comments

--- src/main/java/cavbotics/ntclient/NetworkTableClient.java
+++ src/main/java/cavbotics/ntclient/NetworkTableClient.java
@@ -7,20 +7,28 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 
 @SpringBootApplication
 public class NetworkTableClient {
+    // Team number
     private static int team;
 
     public static void main(String[] args) {
-        if (args.length > 0) team = Integer.parseInt(args[0]);
+        // Handles team number argument
+        if (args.length == 0) team = 8590;
+        else team = Integer.parseInt(args[0]);
+
         SpringApplication.run(NetworkTableClient.class, args);
 
         // Connect to the robot after server is initialized
         //connect(team);
     }
 
+    /**
+     * Connect to the robot's Network Table server
+     * @param team Team number
+     */
     public static void connect(int team) {
         Constants.inst = NetworkTableInstance.getDefault();
         Constants.table = Constants.inst.getTable("datatable");
-        Constants.inst.startClientTeam(team); // where TEAM=190, 294, etc, or use inst.startClient("hostname") or similar
+        Constants.inst.startClientTeam(team);
         Constants.inst.startDSClient(); // recommended if running on DS computer; this gets the robot IP from the DS
     }
 }
--- src/main/java/cavbotics/ntclient/api/NumberController.java
+++ src/main/java/cavbotics/ntclient/api/NumberController.java
@@ -2,7 +2,6 @@ package cavbotics.ntclient.api;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -11,11 +10,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * This file handles the Number routes
+ */
 @RestController
 public class NumberController {
 	@Autowired
     private ObjectMapper objectMapper;
 
+	/**
+	 * Returns the number the key maps to. If the key does not exist or is of different type, it will return the default value (0).
+	 * Equivalent of edu.wpi.first.wpilibj.smartdashboard.SmartDashboard.getNumber
+	 * @param key The key to look up
+	 * @return The value associated with the given key or the given default value if there is no value associated with the key
+	 * @throws JsonProcessingException Exception for formatting and returning JSON
+	 */
 	@GetMapping("/getnumber")
 	public String getnumber(@RequestParam(value = "key", defaultValue = "") String key) throws JsonProcessingException {
 		Number updated = new Number(key);
@@ -23,12 +32,25 @@ public class NumberController {
 		return json;
 	}
 
+	/**
+	 * Updates a number in the table. Adds a new number if the key does not exist.
+	 * Equivalent of edu.wpi.first.wpilibj.smartdashboard.SmartDashboard.putNumber
+	 * @param key The key to be assigned to
+	 * @param num The value that will be assigned
+	 * @return False if the table key already exists with a different type. Otherwise True.
+	 */
 	@PostMapping("/setnumber")
 	public boolean setnumber(@RequestParam(value = "key", defaultValue = "") String key, @RequestParam(value = "value", defaultValue = "") java.lang.Number num) {
 		Number updated = new Number(key, num);
 		return updated.updateNumber();
 	}
 
+	/**
+	 * Deletes the specified key in this table. The key can not be null.
+	 * Equivalent of edu.wpi.first.wpilibj.smartdashboard.SmartDashboard.delete
+	 * @param key The key to delete
+	 * @return True if successful. False otherwise.
+	 */
 	@DeleteMapping("/deletenumber")
 	public boolean deletenumber(@RequestParam(value = "key", defaultValue = "") String key) {
 		Number updated = new Number(key);
