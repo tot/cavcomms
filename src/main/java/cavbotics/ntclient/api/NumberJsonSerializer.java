cd9036e64ae00faef31227567fa8bd6f8dac0241
T <totran0331@gmail.com>
2022-03-12 19:20:23 -0500

    Added documentation for NumberJsonSerializer

--- src/main/java/cavbotics/ntclient/api/NumberJsonSerializer.java
+++ src/main/java/cavbotics/ntclient/api/NumberJsonSerializer.java
@@ -9,8 +9,25 @@ import com.fasterxml.jackson.databind.SerializerProvider;
 
 import org.springframework.boot.jackson.JsonComponent;
 
+/**
+ * Handles converting between Number and JSON
+ */
 @JsonComponent
 public class NumberJsonSerializer extends JsonSerializer<Number> {
+
+  /**
+   * Serializes the Number object into JSON format
+   * 
+   * @param num                Number object to serialize
+   * @param jsonGenerator      JSON generator object from Jackson (provided
+   *                           automatically)
+   * @param serializerProvider Serializer Provider object from Jackson (provided
+   *                           automatically)
+   * 
+   * @throws IOException             Missing at least one parameter
+   * @throws JsonProcessingException Problem(s) encountered when processing to
+   *                                 JSON
+   */
   @Override
   public void serialize(Number num, JsonGenerator jsonGenerator,
       SerializerProvider serializerProvider) throws IOException,
@@ -23,6 +40,12 @@ public class NumberJsonSerializer extends JsonSerializer<Number> {
     jsonGenerator.writeEndObject();
   }
 
+  /**
+   * Converts Number value to String
+   * 
+   * @param num Number value
+   * @return Number value as a String
+   */
   private static String getNumberString(java.lang.Number num) {
     return num.toString();
   }
