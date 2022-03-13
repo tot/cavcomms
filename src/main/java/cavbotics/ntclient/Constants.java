a5c6284d92c99039e18113fe43b8627605d2a8e9
T <totran0331@gmail.com>
2022-03-12 19:14:57 -0500

    Added constants comment

--- src/main/java/cavbotics/ntclient/Constants.java
+++ src/main/java/cavbotics/ntclient/Constants.java
@@ -3,6 +3,9 @@ package cavbotics.ntclient;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
+/**
+ * Constants to be used throughout the program
+ */
 public class Constants {
     public static NetworkTableInstance inst;
     public static NetworkTable table;
